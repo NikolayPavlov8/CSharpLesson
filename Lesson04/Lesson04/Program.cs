a2bf339890b67d06cc8bf4236e81c742bb031b5a
Nikolay Pavlov <wiborgcastle@gmail.com>
2019-06-28 18:56:09 +0300

    code2

--- Lesson04/Lesson04/Program.cs
+++ Lesson04/Lesson04/Program.cs
@@ -1,15 +1,34 @@
-﻿using System;
+﻿// Начало секций подключаемых пространств имен
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lesson04
+
+// объявление нового пространства имен
+namespace Lesson04 
 {
-    class Program
+    class Program       // объявление нового класса
     {
-        static void Main(string[] args)
+        static void Main(string[] args)         // объявление нового метода
         {
-        }
-    }
-}
+
+            Console.Write("Введите свое имя: ");
+
+            // этот метод может считать с консоля строку
+            string name = Console.ReadLine();           // вводим имя
+
+            // затем введенное имя выводится на консоль
+            Console.WriteLine($"Привет {name}");        // выводим имя на консоль
+
+
+            Console.WriteLine("Привет " + name);
+
+            Console.ReadKey();
+
+
+        }       // конец метода
+    }           // конец класса
+}               // конец пространства имен
