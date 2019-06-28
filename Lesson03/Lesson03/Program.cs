e5284bba6d68316bf625f1135a65994b8dffa45c
NikolayPavlov8 <nikolaspavlov8@gmail.com>
2019-06-28 12:17:46 +0300

    code1

---
+++ Lesson03/Lesson03/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson03
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
