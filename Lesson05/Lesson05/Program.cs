1a139e8cf351f222ec4cdde304854970e2395f67
Nikolay Pavlov <wiborgcastle@gmail.com>
2019-06-29 12:05:25 +0300

    code1

---
+++ Lesson05/Lesson05/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string hello = "Привет Мир";
+            Console.WriteLine(hello);
+            Console.WriteLine("Добро пожаловать в C#");
+            Console.WriteLine("Пока мир ...");
+            Console.WriteLine(24.5);
+
+            Console.ReadKey();
+
+            string name = "Tom";
+            int age = 34;
+            double height = 1.7;
+            Console.WriteLine($"Имя: {name} Возраст: {age} Рост {height} м");
+            Console.ReadKey();
+
+
+        }
+    }
+}
