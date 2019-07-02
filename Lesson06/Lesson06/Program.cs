4e553772088751be8baa65c1fdb7ee5f8ab8fdb3
Nikolay Pavlov8 <nikolaspavlov8@gmail.com>
2019-07-02 15:46:01 +0300

    code2

--- Lesson06/Lesson06/Program.cs
+++ Lesson06/Lesson06/Program.cs
@@ -10,11 +10,30 @@ namespace Lesson06
     {
         static void Main(string[] args)
         {
-            string data;
+            //string data;
+            //data = Console.ReadLine();
+            //Console.WriteLine("Привет " + data + "!!!");
 
-            data = Console.ReadLine();
+            string str1 = "5";
+            string str2 = "2";
+            Console.WriteLine(str1 + str2);     //выполнение плюс, выведет 52
+            int a = Convert.ToInt32(str1);
+            int b = Convert.ToInt32(str2);
+            Console.WriteLine(a + b);
 
-            Console.WriteLine("Привет " + data + "!!!");
+            string str;
+            int d, c;
+            Console.WriteLine("Введите число 1");
+            str = Console.ReadLine();
+            d = Convert.ToInt32(str);
+
+            Console.WriteLine("Введите число 2");
+            str = Console.ReadLine();
+            c = Convert.ToInt32(str);
+
+            int result = d + c;
+            Console.WriteLine("Сумма чисел = " + result);
+
         }
     }
 }
