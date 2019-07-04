aeb802071933a2330c3eb9dde3bb85bbe8a9abcb
Nikolay Pavlov8 <nikolaspavlov8@gmail.com>
2019-07-04 18:34:49 +0300

    code2

---
+++ Dom_Lesson/Dom_Lesson03/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * 3. Напишите простой конвертор валют
+ *    (без восможности динамического выбора валюты пользователем).
+ *    Валюты заданы хардкором и не изменяются. Тип валюты на выбор программиста.
+ */
+
+namespace Dom_Lesson03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
