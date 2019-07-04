b7144b8297215b2482733c5fa07e5371384da3fa
Nikolay Pavlov8 <nikolaspavlov8@gmail.com>
2019-07-04 17:00:41 +0300

    code1

--- Lesson08/Lesson081/Program.cs
+++ Lesson08/Lesson081/Program.cs
@@ -4,17 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-/*
- * несколько проектов в одном решении
- */
-
-namespace Lesson08
+namespace Lesson081
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world from Lesson08!");
+            Console.WriteLine("Hello world from Lesson081!");
             Console.ReadLine();
         }
     }
