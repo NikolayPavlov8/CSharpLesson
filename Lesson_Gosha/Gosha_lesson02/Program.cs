353e76bf39e67c0ba8e4d59e1f3cd9c70dbe1ea6
Nikolay Pavlov8 <nikolaspavlov8@gmail.com>
2019-07-08 17:48:05 +0300

    code3

---
+++ Lesson_Gosha/Gosha_lesson02/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gosha_lesson02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "User";
+
+            switch (name)
+            {
+                case "bob":
+                    Console.Write("Имя Боб");
+                    break;
+                case "User":
+                    Console.Write("User");
+                    break;
+                default:
+                    Console.Write("Какое то другое имя");
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
--- Lesson_Gosha/Lesson_Gosha/Program.cs
+++ Lesson_Gosha/Lesson_Gosha/Program.cs
@@ -12,6 +12,28 @@ namespace Lesson_Gosha
         {
             //Console.WriteLine("Hello World");
 
+            short num;
+            bool hasCar = true;
+            Console.Write("Введите первое число: ");
+            num = Convert.ToInt16(Console.ReadLine());
+
+            if (num == 10)
+            {
+                Console.WriteLine("Число равно 10");
+            }
+            else if (num >= 50)
+            {
+                if (num < 100 && hasCar == true)
+                {
+                    Console.WriteLine("Число меньше 100");
+                }
+                Console.WriteLine("Число больше 50");
+            }
+            else
+            {
+                Console.WriteLine("Число неизвестно");
+            }
+
 
 
             Console.ReadKey();
