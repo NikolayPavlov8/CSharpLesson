72bbce94b138146269df078caf605f02a8d9a0d9
Nikolay Pavlov8 <nikolaspavlov8@gmail.com>
2019-07-09 14:08:16 +0300

    code3

--- Lesson_Gosha/Gosha_lesson05/Program.cs
+++ Lesson_Gosha/Gosha_lesson05/Program.cs
@@ -10,6 +10,36 @@ namespace Gosha_lesson05
     {
         static void Main(string[] args)
         {
+            /*
+            for (byte i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Element - " + i);
+            }
+            */
+
+            //Цикл while
+            /*
+            byte j = 0;
+            while(j < 10)
+            {
+                Console.WriteLine("Element - " + j);
+                j++;
+            }
+
+            short h = 1000;
+            while(h > 127)
+            {
+                Console.WriteLine("Element - " + h);
+                h -= 100;
+            }
+            */
+
+            for (byte i = 1; i <= 40; i++)
+            {
+                if (i == 35)
+                    break;
+                Console.WriteLine("EL: " + i);
+            }
         }
     }
 }
