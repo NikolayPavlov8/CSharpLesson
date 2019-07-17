6c19648d5f042e68c41b1a125abc5f124d4be56e
Nikolay Pavlov8 <nikolaspavlov8@gmail.com>
2019-07-17 11:46:24 +0300

    code1

---
+++ C-Sharp-primer/C-Sharp-primer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_primer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Базовые операции String
+
+            string h = "Hello";
+            Console.WriteLine("h = {0}", h);
+            Console.WriteLine("Длина h = {0}", h.Length);
+            Console.WriteLine("h в верхнем регистре = {0}", h.ToUpper());
+            Console.WriteLine("h = {0}", h);
+            Console.WriteLine("h в нижнем регистре = {0}", h.ToLower());
+            Console.WriteLine("h содеожит e? : {0}", h.Contains("е"));
+            Console.WriteLine("Замена {0}", h.Replace("lo", ""));
+        }
+    }
+}
